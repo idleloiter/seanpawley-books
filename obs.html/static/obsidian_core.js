41b64a2bd53181024083e11013ae3a3a09d9c770
Sean Pawley <sean@seanpawley.com>
2026-09-08 04:04:28 -0400

    Vault sync 2026-09-08T08:04:28Z

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -13,7 +13,7 @@ var CONFIG_CLOSE_LEFT_PANE_IF_EMPTY = 0;
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '118338932994896270955482086958245057848'                       // used to check whether the localStorage data is stale
+var gzip_hash = '126187102668023832162551762802716635496'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
